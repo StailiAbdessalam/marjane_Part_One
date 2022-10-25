578a1e618fd921f3932e9d05959d598d86b1785d
abdessalm staili <abdoycode@gmail.com>
2022-10-25 15:01:37 +0100

    docs: make a entity Center in models folder

---
+++ src/main/java/Models/Center.java
@@ -0,0 +1,33 @@
+package Models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "center")
+@Data
+@NoArgsConstructor
+public class Center {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cid", nullable = false)
+    private Integer id;
+
+    @Column(name = "cname", nullable = false)
+    private String cname;
+
+    @Column(name = "cville", nullable = false)
+    private String cville;
+
+    @Column(name = "ccreatedat", nullable = false)
+    private Instant ccreatedat;
+
+    public Center(String cname, String cville, Instant ccreatedat) {
+        this.cname = cname;
+        this.cville = cville;
+        this.ccreatedat = ccreatedat;
+    }
+}
