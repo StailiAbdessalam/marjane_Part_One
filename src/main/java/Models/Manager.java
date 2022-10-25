d232c2a015ddaa92b71592954fddfe0496b9b42d
abdessalm staili <abdoycode@gmail.com>
2022-10-25 15:38:58 +0100

    docs: make a entity Manager in models folder

---
+++ src/main/java/Models/Manager.java
@@ -0,0 +1,41 @@
+package Models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "manager")
+@NoArgsConstructor @Data
+public class Manager {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "mid", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "mcenteradmin", nullable = false)
+    private Centreadmin mcenteradmin;
+
+    @Column(name = "mfullname", nullable = false)
+    private String mfullname;
+
+    @Column(name = "memail", nullable = false)
+    private String memail;
+
+    @Column(name = "mpassword", nullable = false)
+    private String mpassword;
+
+    @Column(name = "mcreatedat", nullable = false)
+    private Instant mcreatedat;
+
+    public Manager(Centreadmin mcenteradmin, String mfullname, String memail, String mpassword, Instant mcreatedat) {
+        this.mcenteradmin = mcenteradmin;
+        this.mfullname = mfullname;
+        this.memail = memail;
+        this.mpassword = mpassword;
+        this.mcreatedat = mcreatedat;
+    }
+}
